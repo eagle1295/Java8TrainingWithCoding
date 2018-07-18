6236ff62cd9153e35a95e306f047a242092a0eae
dilsh0d <dilshod.toj@gmail.com>
2018-07-18 21:12:16 +0500

    change documentation file

--- src/uz/javaworld/samples/les1/HelloJava.java
+++ src/uz/javaworld/samples/les1/HelloJava.java
@@ -4,7 +4,7 @@ package uz.javaworld.samples.les1;// this package name po to class location in t
  * <b>JDK</b>(Java Development Kit) compiled HelloJava file to byte code (machine code).
  * <b>JDK</b> included <b>JRE</b>(Java Runtime Environment) and <b>JVM</b>(Java Virtual Machine)
  *
- * <b>JVM</b> running byte code in the everywhere platform. One write code, everywhere running
+ * <b>JVM</b> running byte code in the everywhere platform. Once write code, everywhere running
  */
 public class HelloJava { // This is class name! Class must begin first character with Uppercase other with lowercase
 
